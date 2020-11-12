63c3b6db2b55b737201478c93ec646aaf552e60c
sunabove <sunabove@nate.com>
2020-11-12 12:24:19 -1000

    **

--- Motor.py
+++ Motor.py
@@ -1,31 +1,50 @@
+#coding: utf-8
 import RPi.GPIO as GPIO
 import time
 
 class Motor:
-    
+
     def __init__(self,ain1=12,ain2=13,ena=6,bin1=20,bin2=21,enb=26):
-        self.AIN1 = ain1
-        self.AIN2 = ain2
-        self.BIN1 = bin1
-        self.BIN2 = bin2
-        self.ENA = ena
-        self.ENB = enb
-        self.PA  = 50
-        self.PB  = 50
+        self.AIN1 = ain1; self.AIN2 = ain2; 
+        self.BIN1 = bin1; self.BIN2 = bin2; 
+        self.PA  = 50 ; self.PB  = 50
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.AIN1,GPIO.OUT)
-        GPIO.setup(self.AIN2,GPIO.OUT)
-        GPIO.setup(self.BIN1,GPIO.OUT)
-        GPIO.setup(self.BIN2,GPIO.OUT)
-        GPIO.setup(self.ENA,GPIO.OUT)
-        GPIO.setup(self.ENB,GPIO.OUT)
-        self.PWMA = GPIO.PWM(self.ENA,500)
-        self.PWMB = GPIO.PWM(self.ENB,500)
+        for port in [ ain1, ain2, ena, bin1, bin2, enb ] : 
+            GPIO.setup(port,GPIO.OUT)
+        pass
+
+        self.PWMA = GPIO.PWM(ena,500)
+        self.PWMB = GPIO.PWM(enb,500)
         self.PWMA.start(self.PA)
         self.PWMB.start(self.PB)
+        
         self.stop()
+    pass
+
+    def setMotor(self, left, right):
+        self.setGPIO_PWN( self.AIN1, self.AIN2, self.PWMA, right )
+        self.setGPIO_PWN( self.BIN1, self.BIN2, self.PWMB, left )
+    pass
+
+    def setGPIO_PWN( self, ain1, ain2, pwma, value ) : 
+        value = 100 if value > 100 else value 
+        value = -100 if value < -100 else value 
+
+        if value > 0 :
+            GPIO.output(ain1,GPIO.HIGH)
+            GPIO.output(ain2,GPIO.LOW)
+            pwma.ChangeDutyCycle(value)
+        elif value < 0 :
+            GPIO.output(ain1,GPIO.LOW)
+            GPIO.output(ain2,GPIO.HIGH)
+            pwma.ChangeDutyCycle( - value)
+        else :
+            pwma.ChangeDutyCycle( 0 )
+            GPIO.output(ain1,GPIO.LOW)
+            GPIO.output(ain2,GPIO.LOW)
+        pass 
     pass
 
     def forward(self, value=50):
@@ -52,30 +71,8 @@ class Motor:
         print("right")
         self.setMotor( -30, 30 )
     pass
-        
-    def setMotor(self, left, right):
-        self.setGPIO_PWN( self.AIN1, self.AIN2, self.PWMA, right )
-        self.setGPIO_PWN( self.BIN1, self.BIN2, self.PWMB, left )
-    pass
 
-    def setGPIO_PWN( self, ain1, ain2, pwma, value ) : 
-        value = 100 if value > 100 else value 
-        value = -100 if value < -100 else value 
-
-        if value > 0 :
-            GPIO.output(ain1,GPIO.HIGH)
-            GPIO.output(ain2,GPIO.LOW)
-            pwma.ChangeDutyCycle(value)
-        elif value < 0 :
-            GPIO.output(ain1,GPIO.LOW)
-            GPIO.output(ain2,GPIO.HIGH)
-            pwma.ChangeDutyCycle( - value)
-        else :
-            pwma.ChangeDutyCycle( 0 )
-            GPIO.output(ain1,GPIO.LOW)
-            GPIO.output(ain2,GPIO.LOW)
-        pass 
-    pass
+pass
 
 if __name__=='__main__':
 
