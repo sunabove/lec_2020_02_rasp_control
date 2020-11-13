c0bf97cd16add89cb5c50a7c8fe853a67650b32c
sunabove <sunabove@nate.com>
2020-11-13 13:19:04 -1000

    **

--- Camera.py
+++ Camera.py
@@ -1,59 +1,86 @@
 #coding: utf-8
 
-import io
-import picamera
-import logging
-from threading import Condition
+import cv2
+import numpy as np 
 
-class StreamingOutput(object):
+class Camera(object):
     def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
+        self.video = cv2.VideoCapture(0)
+        self.video.set(cv2.CAP_PROP_FPS, 15)
+        self.video.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'X264'))
     pass
-pass
-
-class Camera :
-    def __init__(self) :
-        self.cam = picamera.PiCamera(resolution='640x480', framerate=24)
-        self.output = None 
+    
+    def __del__(self):
+        self.video.release()
     pass
 
-    def __del__(self) :
-        cam.stop_recording()
-    pass
+    def get_image(self):
+        global ads
+        car = ads.car 
+        gps = ads.gps
 
-    def get_frame(self) :
-        if not self.output :
-            self.output = StreamingOutput()
-            self.cam.start_recording(self.output, format='mjpeg')
+        success, img = self.video.read()
+
+        if not success :
+            h = 480
+            w= 640
+            # black blank image
+            img = np.zeros(shape=[h, w, 3], dtype=np.uint8)
+            pass 
         pass
 
-        output = self.output
-        if output.condition :
-            output.condition.wait()
-            return ouput.frame
-        else :
-            return None 
+        img = cv2.flip( img, 0 )
+        h, w, _ = img.shape # image height, width 
+
+        x = 10   # text x position
+        y = 20   # text y position
+        h = 20   # line height
+
+        txt = "Hello"
+        self.putTextLine( img, txt , x, y )
+
+        return img
+    pass
+
+    def get_frame( self ) : 
+        # get video frame
+
+        img = self.get_image()
+         
+        _, jpg = cv2.imencode('.jpg', img) 
+        
+        return jpg.tobytes()
+    pass
+
+    # opencv 이미지에 텍스트를 그린다.
+    def putTextLine(self, img, txt, x, y ) :
+        # /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf
+        font = cv2.FONT_HERSHEY_SIMPLEX # font
+        
+        fs = 0.4  # font size(scale)
+        ft = 1    # font thickness 
+
+        bg_color = (255, 255, 255) # text background color
+        fg_color = (255,   0,   0) # text foreground color
+
+        cv2.putText(img, txt, (x, y), font, fs, bg_color, ft + 2, cv2.LINE_AA)
+        cv2.putText(img, txt, (x, y), font, fs, fg_color, ft    , cv2.LINE_AA) 
     pass
 pass
 
 if __name__=='__main__':
-    camera = Camera()
+    camera = Camera() 
 
-    while True :
+    while(True):
+        # Capture frame-by-frame
+        image = camera.get_image()
+
+        # Display the resulting frame
+        cv2.imshow('frame', image )
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         pass
     pass
 
+    cv2.destroyAllWindows()
 pass
