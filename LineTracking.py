037521d5dfd8ecb51210ecf9bbea86456d8cc6c7
sunabove <sunabove@nate.com>
2020-12-11 13:58:47 -1000

    **

--- LineTracking.py
+++ LineTracking.py
@@ -18,7 +18,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)	
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)    
 
 maximum = 100;
 j = 0
@@ -26,16 +26,18 @@ integral = 0;
 last_proportional = 0
 
 def Wheel(pos):
-	# """Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
-	pass
+    pos = int( pos )
+    
+    # """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+    pass
 pass
 
 # Create NeoPixel object with appropriate configuration.
@@ -57,18 +59,18 @@ print("Line follow Example")
 
 time.sleep(0.5)
 
-for i in range(0,100):
-	if(i<25 or i>= 75):
-		Ab.right()
-		Ab.setPWMA(30)
-		Ab.setPWMB(30)
-	else:
-		Ab.left()
-		Ab.setPWMA(30)
-		Ab.setPWMB(30)
-	pass
+for i in range(0, 10):
+    if(i<25 or i>= 75):
+        Ab.right()
+        Ab.setPWMA(30)
+        Ab.setPWMB(30)
+    else:
+        Ab.left()
+        Ab.setPWMA(30)
+        Ab.setPWMB(30)
+    pass
 
-	TR.calibrate()
+    TR.calibrate()
 pass
 
 Ab.stop()
@@ -78,66 +80,66 @@ print(TR.calibratedMax)
 
 #while (GPIO.input(Button) != 0):
 for i in range( 100 ) :
-	position,Sensors = TR.readLine()
-	print(position,Sensors)
-	time.sleep(0.05)
+    position,Sensors = TR.readLine()
+    print(position,Sensors)
+    time.sleep(0.05)
 pass
 
 Ab.forward()
 
 while True:
-	position,Sensors = TR.readLine()
-	#print(position)
-	if(Sensors[0] >900 and Sensors[1] >900 and Sensors[2] >900 and Sensors[3] >900 and Sensors[4] >900):
-		Ab.setPWMA(0)
-		Ab.setPWMB(0);
-	else:
-		# The "proportional" term should be 0 when we are on the line.
-		proportional = position - 2000
-		
-		# Compute the derivative (change) and integral (sum) of the position.
-		derivative = proportional - last_proportional
-		integral += proportional
-		
-		# Remember the last position.
-		last_proportional = proportional
+    position,Sensors = TR.readLine()
+    #print(position)
+    if(Sensors[0] >900 and Sensors[1] >900 and Sensors[2] >900 and Sensors[3] >900 and Sensors[4] >900):
+        Ab.setPWMA(0)
+        Ab.setPWMB(0);
+    else:
+        # The "proportional" term should be 0 when we are on the line.
+        proportional = position - 2000
+        
+        # Compute the derivative (change) and integral (sum) of the position.
+        derivative = proportional - last_proportional
+        integral += proportional
+        
+        # Remember the last position.
+        last_proportional = proportional
 
-		'''
-		// Compute the difference between the two motor power settings,
-		// m1 - m2.  If this is a positive number the robot will turn
-		// to the right.  If it is a negative number, the robot will
-		// turn to the left, and the magnitude of the number determines
-		// the sharpness of the turn.  You can adjust the constants by which
-		// the proportional, integral, and derivative terms are multiplied to
-		// improve performance.
-		'''
-		power_difference = proportional/30  + integral/10000 + derivative*2;  
+        '''
+        // Compute the difference between the two motor power settings,
+        // m1 - m2.  If this is a positive number the robot will turn
+        // to the right.  If it is a negative number, the robot will
+        // turn to the left, and the magnitude of the number determines
+        // the sharpness of the turn.  You can adjust the constants by which
+        // the proportional, integral, and derivative terms are multiplied to
+        // improve performance.
+        '''
+        power_difference = proportional/30  + integral/10000 + derivative*2;  
 
-		if (power_difference > maximum):
-			power_difference = maximum
-		elif (power_difference < - maximum):
-			power_difference = - maximum
-		pass
+        if (power_difference > maximum):
+            power_difference = maximum
+        elif (power_difference < - maximum):
+            power_difference = - maximum
+        pass
 
-		print(position,power_difference)
+        print(position,power_difference)
 
-		if (power_difference < 0):
-			Ab.setPWMA(maximum + power_difference)
-			Ab.setPWMB(maximum);
-		else:
-			Ab.setPWMA(maximum);
-			Ab.setPWMB(maximum - power_difference)
-		pass
-		
-	for i in range(0,strip.numPixels()):
-		strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255))
-	pass
+        if (power_difference < 0):
+            Ab.setPWMA(maximum + power_difference)
+            Ab.setPWMB(maximum);
+        else:
+            Ab.setPWMA(maximum);
+            Ab.setPWMB(maximum - power_difference)
+        pass
+        
+    for i in range(0,strip.numPixels()):
+        strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255))
+    pass
 
-	strip.show();
-	j += 1
+    strip.show();
+    j += 1
 
-	if(j > 256*4): 
-		j= 0
-	pass
+    if(j > 256*4): 
+        j= 0
+    pass
 
 pass
