a8b7bbb1d3dc2f590776f6d5b4e085ff56ab84dd
openvisual <67694546+openvisual@users.noreply.github.com>
2020-12-15 12:57:31 +0900

    Create tst_plot_realtime.py

---
+++ test/tst_plot_realtime.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#from IPython.display import display, clear_output
+
+if False :
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    for i in range(20):
+        x = np.arange(0, i, 0.1);
+        y = np.sin(x)
+
+        ax.set_xlim(0, i)
+
+        ax.cla()
+        ax.plot(x, y)
+
+        plt.pause(0.5)
+    pass
+pass
+
+if True :
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    for i in range(21):
+        ax.set_xlim(0, 20)
+
+        ax.plot(i, 1, marker='x')
+
+        plt.pause(0.5)
+    pass
+pass
