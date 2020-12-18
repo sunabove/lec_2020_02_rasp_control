419a3c62e84c4023d8d618b802e34a7dea7b8108
sunabove <sunabove@nate.com>
2020-12-18 13:20:06 -1000

    **

--- LineTrackingPID.py
+++ LineTrackingPID.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO, threading, signal, inspect
 import numpy as np
 from random import random
 from time import sleep, time
+from gpiozero import Buzzer
 from TRSensor import TRSensor
 
 import logging as log
@@ -14,10 +15,16 @@ log.basicConfig(
 
 class LineTracker :
 
-    def __init__(self, robot, white = 570, black=240 ):
+    def __init__(self, robot, white = 570, black=240, buzzer = None ):
         self.robot = robot
         self.white = white
         self.black = black 
+
+        if buzzer is None : 
+            self.buzzer = Buzzer(4)
+        elif buzzer is not None : 
+            self.buzzer = buzzer
+        pass
 
         self._running = False  
     pass
@@ -66,7 +73,12 @@ class LineTracker :
         self._running = True 
 
         robot = self.robot 
+        buzzer = self.buzzer
 
+        # 시작음
+        buzzer.beep(on_time=1, off_time=0.2, n = 3, background=False)
+
+        # 주행 센서
         tr = TRSensor(white=self.white, black=self.black)
 
         then = time()
