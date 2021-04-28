e7245ebbe317e5dc6b23caca233ec6878fb50f51
sunabove <sunabove@nate.com>
2021-04-29 00:46:37 +0100

    **

--- camera/camera_dark.py
+++ camera/camera_dark.py
@@ -4,15 +4,13 @@ from time import sleep
 from fractions import Fraction
 
 with picamera.PiCamera() as camera:
-    # Set a framerate of 1/6fps, then set shutter
-    # speed to 6s and ISO to 800
     camera.framerate = Fraction(1, 6)
-    camera.shutter_speed = 6000
+    camera.shutter_speed = 200 #miliseconds
     camera.exposure_mode = 'off'
     camera.iso = 400 #max 800
     # Give the camera a good long time to measure AWB
     # (you may wish to use fixed AWB instead)
-    sleep(10)
+    sleep(1)
     # Finally, capture an image with a 6s exposure. Due
     # to mode switching on the still port, this will take
     # longer than 6 seconds
