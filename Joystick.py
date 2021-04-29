76a4ac57629cd0473d63656dfe03a1c9707637af
sunabove <sunabove@nate.com>
2021-04-29 01:24:28 +0100

    **

--- Joystick.py
+++ Joystick.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
-import time
-from AlphaBot2 import AlphaBot2
+from time import sleep
+from Motor import Motor
 
-Ab = AlphaBot2()
+motor = Motor()
 
 CTR = 7
 A = 8
@@ -12,53 +12,57 @@ D = 11
 BUZ = 4
 
 def beep_on():
-	GPIO.output(BUZ,GPIO.HIGH)
+    GPIO.output(BUZ,GPIO.HIGH)
+pass
+
 def beep_off():
-	GPIO.output(BUZ,GPIO.LOW)
-	
+    GPIO.output(BUZ,GPIO.LOW)
+pass
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(CTR,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(A,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(B,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(C,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(D,GPIO.IN,GPIO.PUD_UP)
 GPIO.setup(BUZ,GPIO.OUT)
+for key in [ CTR, A, B, C, D ] : 
+    GPIO.setup(key,GPIO.IN,GPIO.PUD_UP)
+pass
 
 try:
-	while True:
-		if GPIO.input(CTR) == 0:
-			beep_on();
-			Ab.stop();
-			print("center")
-			while GPIO.input(CTR) == 0:
-				time.sleep(0.01)
-		elif GPIO.input(A) == 0:
-			beep_on();
-			Ab.forward();
-			print("up")
-			while GPIO.input(A) == 0:
-				time.sleep(0.01)
-		elif GPIO.input(B) == 0:
-			beep_on();
-			Ab.right();
-			print("right")
-			while GPIO.input(B) == 0:
-				time.sleep(0.01)
-		elif GPIO.input(C) == 0:
-			beep_on();
-			Ab.left();
-			print("left")
-			while GPIO.input(C) == 0:
-				time.sleep(0.01)
-		elif GPIO.input(D) == 0:
-			beep_on();
-			Ab.backward();
-			print("down")
-			while GPIO.input(D) == 0:
-				time.sleep(0.01)
-		else:
-			beep_off();
+    print( "JoyStick is ready to control." )
+    while 1 :
+        key = None 
+        if GPIO.input(CTR) == 0:
+            print("center")
+            key = CTR 
+            beep_on();
+            motor.stop();
+        elif GPIO.input(A) == 0:
+            print("up")
+            key = A
+            beep_on();
+            motor.forward();            
+        elif GPIO.input(B) == 0:
+            print("right")
+            key = B
+            beep_on();
+            motor.right();
+        elif GPIO.input(C) == 0:
+            print("left")
+            key = C
+            beep_on();
+            motor.left();
+        elif GPIO.input(D) == 0:
+            print("down")
+            key = D
+            beep_on();
+            motor.backward();
+        else:
+            beep_off();
+        pass
 
+        while key and GPIO.input(key) == 0:
+            sleep(0.01)
+        pass
+    pass
 except KeyboardInterrupt:
-	GPIO.cleanup()
+    GPIO.cleanup()
+pass
