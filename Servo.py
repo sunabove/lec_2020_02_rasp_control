671c3050545c4e0f854520dea7240672c4b1450b
sunabove <sunabove@nate.com>
2021-04-30 19:47:03 +0100

    **

--- Servo.py
+++ Servo.py
@@ -9,19 +9,14 @@ class Servo :
     # ============================================================================
 
     # Registers/etc.
-    __SUBADR1          = 0x02
-    __SUBADR2          = 0x03
-    __SUBADR3          = 0x04
+    
     __MODE1            = 0x00
     __PRESCALE         = 0xFE
+    
     __LED0_ON_L        = 0x06
     __LED0_ON_H        = 0x07
     __LED0_OFF_L       = 0x08
     __LED0_OFF_H       = 0x09
-    __ALLLED_ON_L      = 0xFA
-    __ALLLED_ON_H      = 0xFB
-    __ALLLED_OFF_L     = 0xFC
-    __ALLLED_OFF_H     = 0xFD
 
     def __init__(self, address=0x40, debug=0):
         self.bus = smbus.SMBus(1)
