e56e5509e3034f66e20163bcd24727afff29391b
sunabove <sunabove@nate.com>
2021-05-04 19:06:42 +0100

    **

--- Functions.py
+++ Functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from gpiozero import Button
+from gpiozero import Button, Buzzer
 from time import sleep
 import os
 
@@ -14,8 +14,25 @@ def blink_internal_led(led_no=0) :
     pass
 pass
 
+def beep_warning() :
+    buzzer = Buzzer(4)
+
+    cnt = 9 
+    for frq in range( 1, cnt ) : 
+        frq = cnt - frq
+        t = 1/frq
+        buzzer.beep(on_time=t, off_time=t/4, n = int(frq), background=False)
+        sleep( 1 )
+    pass
+
+    buzzer.beep(on_time=5, off_time=1, n = 1, background=False)
+    sleep( 1 )
+pass # -- beep_warning
+
 def shutdown():
     print( '\nShutdown now ...' )
+    beep_warning()
+    
     blink_internal_led( 0 )
     blink_internal_led( 1 )
 
