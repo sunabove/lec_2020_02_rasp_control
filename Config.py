b6e61e592b2c184abe449353dd08a2d1a903fb92
sunabove <sunabove@nate.com>
2021-05-16 15:27:07 +0100

    **

--- Config.py
+++ Config.py
@@ -3,46 +3,70 @@
 import configparser, os, io
 
 config = None
-robot_section = {}
+robot_section = None
 section_name = "robot"
 configfile_name = "robot.config"
 
-def cfg(key, v=None) :
+def cfg(key, v=None, debug=0) :
 
-    # Check if there is already a configurtion file
-    if not os.path.isfile( configfile_name ):
-        # Create the configuration file as it doesn't exist yet
-        cfgfile = open( configfile_name, "w" )
+    global robot_section
+    global config
 
-        # Add content to the file
-        config = configparser.ConfigParser()
-        
-        robot_section = {}
-        robot_section[ "name" ] = "alphabot"
+    if robot_section is None :
+        # Check if there is already a configurtion file
+        if not os.path.isfile( configfile_name ):
+            # Create the configuration file as it doesn't exist yet
+            cfgfile = open( configfile_name, "w" )
 
-        config[ section_name ] = robot_section
+            # Add content to the file
+            config = configparser.ConfigParser()
+            
+            robot_section = {}
+            robot_section[ "name" ] = "alphabot"
 
-        config.write(cfgfile)
+            config[ section_name ] = robot_section
 
-        cfgfile.close()
-    pass
+            config.write(cfgfile)
 
-    # Load the configuration file
-    with open( configfile_name ) as f:
-        sample_config = f.read()
-        config = configparser.ConfigParser()
-        config.read( configfile_name )
+            cfgfile.close()
+        else :
+            # Load the configuration file
+            with open( configfile_name ) as f:
+                config = configparser.ConfigParser()
+                config.read( configfile_name )
 
-        robot_section = config[ section_name ]
+                robot_section = config[ section_name ]
 
-        for key in robot_section :
-            v = robot_section[ key ]
-            t = f"{key} : {v}"
-            print( t )
+                if 0 :
+                    for key in robot_section :
+                        v = robot_section[ key ]
+                        t = f"{key} : {v}"
+                        print( t )
+                    pass
+                pass
+            pass
         pass
     pass
+
+    if robot_section is not None :
+        if key in robot_section :
+            v = robot_section[ key ]
+        elif v is not None :
+            debug and print( f"save ...{key} = {v}" )
+
+            robot_section[key] = v
+
+            cfgfile = open( configfile_name, "w" )
+            config[ section_name ] = robot_section
+            config.write(cfgfile)
+            cfgfile.close()
+        pass
+    pass
+
+    return v
 pass
 
 if __name__ == '__main__':
-    cfg( "", "" )
+    v = cfg( "pid", [6,1,4], debug=1 )
+    print( v )
 pass
