0697ba44cf004b6898944734527634d6c2e3a1cf
sunabove <sunabove@nate.com>
2021-06-11 20:22:59 +0100

    **

--- LineCamera.py
+++ LineCamera.py
@@ -260,7 +260,7 @@ class LineCamera( LineTracker ) :
             max_poly_min_box = np.int0(max_poly_min_box)
 
             cv.drawContours(image, [max_poly_min_box], -1, violet, line_width + 2, cv.LINE_AA, offset=(rmw, rmh))
-            cv.drawContours(image_draw, [max_poly], -1, green, line_width, cv.LINE_AA)
+            cv.drawContours(image, [max_poly], -1, green, line_width, cv.LINE_AA, offset=(rmw, rmh))
         pass
 
         if max_poly is not None :
